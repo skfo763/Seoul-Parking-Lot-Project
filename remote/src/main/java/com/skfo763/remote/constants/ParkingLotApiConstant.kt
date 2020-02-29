b25e68cb7677ebbc26ef8803bed0afe0b26d5703
Changyeon Seo <scy7351@naver.com>
2020-03-01 01:03:18 +0900

    [0003] Api key 보안 취약점 삭제

--- remote/src/main/java/com/skfo763/remote/constants/ParkingLotApiConstant.kt
+++
@@ -1,8 +0,0 @@
-package com.skfo763.remote.constants
-
-class ParkingLotApiConstant {
-    companion object {
-        const val API_KEY = "61536e77457363793834796c564b79"
-        const val OPEN_API_URL = "http://openapi.seoul.go.kr:8088/"
-    }
-}
