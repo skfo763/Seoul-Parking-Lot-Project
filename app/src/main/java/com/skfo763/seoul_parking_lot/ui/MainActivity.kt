3d644176d80212f89caba753022b318e5d9b1652
Changyeon-Seo <scy7351@naver.com>
2020-03-02 00:04:31 +0900

    [0011] 누락된 변경점 추가

--- app/src/main/java/com/skfo763/seoul_parking_lot/ui/MainActivity.kt
+++ app/src/main/java/com/skfo763/seoul_parking_lot/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.skfo763.seoul_parking_lot.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.skfo763.seoul_parking_lot.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,7 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigation()
+    }
 
-        textview.text = "hello~~"
+     private fun setUpNavigation() {
+         val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fg_home_nav_container) as NavHostFragment
+
+         NavigationUI.setupWithNavController(
+             bnv_main_bottom_navigation_bar, navHostFragment.navController)
     }
 }
--- app/src/main/java/com/skfo763/seoul_parking_lot/ui/MapFragment.kt
+++ app/src/main/java/com/skfo763/seoul_parking_lot/ui/MapFragment.kt
@@ -1,10 +1,11 @@
-package com.skfo763.seoul_parking_lot
+package com.skfo763.seoul_parking_lot.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.skfo763.seoul_parking_lot.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
